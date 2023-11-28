374c36af1309c4d376e3abcd3b121c7097f9a1cf
apurvago <80999979+apurvago@users.noreply.github.com>
2023-11-28 22:40:03 +0530

    loginf.py

--- loginf.py
+++ loginf.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_mysql_connector import MySQL
+from flask import Flask, render_template, request, redirect, url_for 
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root1234'
-app.config['MYSQL_DB'] = 'flaskdb'
+app.config['MYSQL_PASSWORD'] = 'apurva1234'
+app.config['MYSQL_DB'] = 'flaskdb1'
 
 mysql = MySQL(app)
 
@@ -14,7 +14,7 @@ mysql = MySQL(app)
 def func1():
     return render_template('loginf.html')
 
-@app.route('/loginsql', methods=['POST', 'GET'])
+@app.route('/loginf', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
@@ -23,8 +23,7 @@ def login():
         cursor.execute("SELECT * FROM flasktable WHERE user_id=%s AND password_user=%s", (username, password))
         account = cursor.fetchone()
         if account:
-            return render_template('Aboutme.html')
-            return jsonify ({"username": request.form.get("username"), "password": request.form.get("password")}, "Login successful")
+            return render_template('aboutme.html')
         else:
             return redirect(url_for("fail"))
 
